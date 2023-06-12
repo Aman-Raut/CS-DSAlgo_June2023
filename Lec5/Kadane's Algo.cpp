877acafdc11de9a518692f9c9f766140ec53e162
sachin235 <32926581+sachin235@users.noreply.github.com>
2023-06-12 20:48:57 +0530

    array practice

---
+++ Lec5/Kadane's Algo.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int a[100];
+	//Input
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+
+	//Kadane's Algorithm
+	int cs=0,ms=0;
+	for(int i=0;i<n;i++){
+		cs += a[i];
+		cs = cs<0?0:cs;
+		ms = max(ms,cs);
+
+	}
+	cout<<ms<<endl;
+
+	return 0;
+}
